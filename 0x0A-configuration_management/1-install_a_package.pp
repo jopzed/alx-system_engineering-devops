03b1e8f668af2b83f6c3556c82a20c3a0bd8cabf
myname <myemail@gmail.com>
2023-10-07 13:33:48 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#This code will install puppet
-package { 'flask':
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
