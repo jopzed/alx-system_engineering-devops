47d5abd5348825316fa1143595c2236520ca3eea
myname <myemail@gmail.com>
2023-10-07 12:49:08 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install puppet-lint -v 2.5.0
+# install puppet-lint -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'flask':
+ensure => '2.1.0'
+provider => 'pip3'
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
